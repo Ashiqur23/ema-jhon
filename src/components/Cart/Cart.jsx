7226c2280da56c166dfc47a611544538df3ae1f3
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-03-28 14:56:54 +0600

    added cart components and calculate

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = ({cart}) => {
+    // const {cart} = props;
+    // console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = total * 5 / 100;
+    const grandTotal = parseInt(total + tax + shipping)
+  return (
+    <div className="cart">
+      <h4>oder summary</h4>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${shipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal}</h6>
+    </div>
+  );
+};
+
+export default Cart;
