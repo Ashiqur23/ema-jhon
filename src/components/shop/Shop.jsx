a10a3f08f0fe1262aa77b6050956efc37bc6b949
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-03-28 15:54:19 +0600

    save cart to local storage

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,38 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart , setCart] = useState([])
-    useEffect(() =>{
-        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    } , [])
-    const handleAddToCart = (product) =>{
-        const newCart = [...cart, product];
-        setCart(newCart)
-    }
-    return (
-        <div className='shop-Container'>
-            <div className="products-container">
-                
-                {
-                    products.map(product =>
-                        <Product
-                         {...product}
-                         key = {product.id}
-                         handleAddToCart = {handleAddToCart}>
-                         </Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  //   fetch api and set State
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  //   get shopping cart in local storage
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+  }, []);
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
+  return (
+    <div className="shop-Container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            {...product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
